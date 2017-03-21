68ba8020676f236ddfdb2cea6c4614b1cfff96ca
Magni77 <michalmagni@gmail.com>
2017-03-21 21:29:38 +0100

    Refactor and property setter

--- erlang.py
+++ erlang.py
@@ -1,30 +1,34 @@
+from math import factorial
+
+
 class ErlangAlgorithm():
 
     def __init__(self, model):
         self.obj = model
 
-    def silnia(self, n):
-        if n > 1:
-            return n * self.silnia(n - 1)
-        else:
-            return 1
-
     def show(self):
         print(self.obj.users)
 
+    def get_obj_lines_and_traffic(self):
+       # print(self.obj.users, self.obj.block, self.obj.traffic)
+        return self.obj.lines, self.obj.users * self.obj.block
+
     def calculate(self):
-        N = self.obj.servers
-        B = self.obj.block
-        A = self.obj.users * self.obj.blocking_rate
-        tab = [x for x in range(N+1)]
-        a = A ** N
-        b = self.silnia(N)
+        """
+        Default algorithm for small numbers
+        :return: blocking rate
+        """
+        n, a = self.get_obj_lines_and_traffic()
+
+        tab = [x for x in range(n+1)]
+        exponent = a ** n
+        b = factorial(n)
 
         c = 0
 
         for idx, val in enumerate(tab):
-            c += (A ** idx) / self.silnia(idx)
+            c += (a ** idx) / factorial(idx)
 
-        d = a / b / c
+        d = exponent / b / c
         print(d)
         return d
--- main.py
+++ main.py
@@ -6,42 +6,31 @@ class MakeUpSomething():
     """
     Model dla wspolczynnika blokady
 
-    blocking_rate = the mean arrival rate of new calls
-    users = the mean call length or holding time
-    traffic = the traffic in Erlangs.
+    users = amount of users / the mean call length or holding time
+    lines =  the number of lines in a trunk group / liczba punktow obslugi
+    blocking_rate = wspolczynnik blokady
 
-    serves = liczba punktow obslugi
+    block = the mean arrival rate of new calls
+    traffic = the traffic in Erlangs = users * block
 
-    block = wspolczynnik blokady
     """
-    def __init__(self, users, servers, block, blocking_rate=0.1):
-        """
-        :param users: Amount of user's using web
-        :param servers: Amount of service points
-        :param block: Wspolczynnik blokady
-        :param blocking_rate: the mean arrival rate of new calls
-        """
-        self.users = users
-        self.blocking_rate = blocking_rate
-        self.servers = servers
+    def __init__(self, users, lines, blocking_rate, block=0.1):
         self.block = block
-        self.traffic = self.users * self.blocking_rate #bussy hour traffic
+
+        self.users = users
+        self.lines = lines
+        self.blocking_rate = blocking_rate
+        self.traffic = self.users * self.block #bussy hour traffic
         self.unknown_value = []
 
-    def set_users(self, new_users):
-        self.users = new_users
+    @property
+    def users(self):
+        return self._users
 
-    def set_servers(self, new_servers):
-        self.servers = new_servers
-
-    def set_block(self, new_block):
-        self.block = new_block
-
-    def set_blocking_rate(self, new_rate):
-        self.blocking_rate = new_rate
-
-    def set_custom_traffic(self, new_traffic):
-        self.traffic = new_traffic
+    @users.setter
+    def users(self, value):
+        self.traffic = value * self.block
+        self._users = value
 
 
 def silnia(n):
@@ -49,34 +38,14 @@ def silnia(n):
         return n * silnia(n - 1)
     else:
         return 1
-#
-# N = 10
-# B = 0.002
-# A = 3.42
-#
-# tab = [x for x in range(N+1)]
-# print(tab)
-# a = A**N
-# b = silnia(N)
-#
-# c = 0
-#
-# for idx, val in enumerate(tab):
-#
-#     c += (A**idx)/silnia(idx)
-#     print('{} = {} do potegi {} podzielic przez {}'.format(c, val, idx, silnia(idx)))
-#
-# d = a/b/c
-# print(d)
-#
-#
-# b = 0.4510370834892845
 
-obj = MakeUpSomething(100, 10, False)
+
+obj = MakeUpSomething(34, 10, False)
 
 algo = ErlangAlgorithm(obj)
 
 algo.calculate()
-
-obj.set_users(200)
+obj.users = 50
+obj.lines = 10
 algo.calculate()
+
