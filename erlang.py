43d6c23103a04c2e7d9b497e7d36042dbad0b0d8
Magni77 <michalmagni@gmail.com>
2017-03-23 17:10:43 +0100

    calculating unit of traffic

--- erlang.py
+++ erlang.py
@@ -1,67 +1,72 @@
 from math import factorial
 
 
-class ErlangAlgorithm():
+class ErlangAlgorithm:
 
     def __init__(self, model):
         self.obj = model
-
-    def show(self):
-        print(self.obj.users)
 
     def get_obj_lines_and_traffic(self):
         """
         Getting data from object
         :return: lines, traffic, blocking_rate
         """
-     #   return self.obj.lines, self.obj.users * self.obj.block, self.obj.blocking_rate
         return self.obj.lines, self.obj.traffic, self.obj.blocking_rate
 
     def calculate(self):
         """
         Checks which parameter is false and call right function
         """
-        n, a, p = self.get_obj_lines_and_traffic()
+        lines, traffic, block = self.get_obj_lines_and_traffic()
 
-        if p is False:
-            return self.calculate_erlang_p(a, n)
-        elif n is False:
-            return self.calculate_erlang_n(a, p)
-        elif a is False:
-            pass
+        if block is False:
+            return self.calculate_erlang_p(traffic, lines)
+        elif lines is False:
+            return self.calculate_erlang_n(traffic, block)
+        elif traffic is False:
+            return self.calculate_erlang_a(lines, block)
 
-    def calculate_erlang_p(self, a, n):
+    def calculate_erlang_p(self, traffic, lines):
         """
         Default erlang algorithm, calculate blocking rate
-        :param a: the traffic in Erlangs
-        :param n: the number of lines
+        :param traffic: the traffic in Erlangs
+        :param lines: the number of lines
         :return: blocking rate
         """
-        tab = [x for x in range(n+1)]
+        tab = [x for x in range(lines + 1)]
 
         _sum = 0
         for idx, val in enumerate(tab):
-            _sum += self.ann(idx, a)
+            _sum += self.ann(idx, traffic)
 
-        p = self.ann(n, a) / _sum
+        Pb = self.ann(lines, traffic) / _sum
 
-        return p
+        return Pb
 
-    def calculate_erlang_n(self, a, p):
+    def calculate_erlang_n(self, traffic, Pb):
         """
         calculate the number of lines in a trunk group
-        :param a: the traffic in Erlangs
-        :param p: blocking rate
+        :param traffic: the traffic in Erlangs
+        :param Pb: blocking rate
         :return: number of lines
         """
-        _n = 1
+        lines = 1
+        p_search = Pb
+        while p_search >= Pb:
+            lines += 1
+            p_search = self.calculate_erlang_p(traffic, lines)
 
-        p_search = self.calculate_erlang_p(a, _n)
-        while p_search >= p:
-            _n += 1
-            p_search = self.calculate_erlang_p(a, _n)
+        return lines-1
 
-        return _n-1
+    def calculate_erlang_a(self, n, Pb):
+        """
+        calculate traffic in erlangs
+        :param n: the number of lines
+        :param Pb: blocking rate
+        :return: number of lines
+        """
+
+        return "Not implemented yet. Sorry"
 
     def ann_small(self, n, a):
         """
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import math
 from erlang import ErlangAlgorithm
 
 
-class MakeUpSomething():
+class MakeUpSomething:
     """
-    Model dla wspolczynnika blokady
+    Model for ErlangB algorithm
 
     users = amount of users / the mean call length or holding time
-    lines =  the number of lines in a trunk group / liczba punktow obslugi
-    blocking_rate = wspolczynnik blokady
+    lines =  the number of servers(lines) in a trunk group / liczba punktow obslugi
+    blocking_rate = Probability of blocking
 
     block = the mean arrival rate of new calls
     traffic = the traffic in Erlangs = users * block
@@ -16,12 +16,22 @@ class MakeUpSomething():
     """
     def __init__(self, users, lines, blocking_rate, block=0.1):
         self.block = block
-
         self.users = users
         self.lines = lines
         self.blocking_rate = blocking_rate
         self.traffic = self.users * self.block #bussy hour traffic
         self.unknown_value = []
+
+    def calculate_traffic_per_hour(self, calls, duration, seconds=True):
+        """ Calculate the total traffic volume of one hour.
+        :param calls: amount of calls made in hour
+        :param duration: average duration time in seconds or minutes
+        :param seconds: If duration in seconds set True, if in minutes set as False
+        :return: Erlang traffic representation
+        """
+        if seconds:
+            return calls*duration/3600
+        return calls*duration/60
 
     @property
     def users(self):
@@ -29,21 +39,28 @@ class MakeUpSomething():
 
     @users.setter
     def users(self, value):
-        self.traffic = value * self.block
+        if value is False:
+            self.traffic = False
+        else:
+            self.traffic = value * self.block
         self._users = value
 
 
-obj = MakeUpSomething(34, 10, False)
-
+obj = MakeUpSomething(users=17.986, lines=False, blocking_rate=0.01)
+obj.traffic = 17.986
 algo = ErlangAlgorithm(obj)
+#print(obj.calculate_traffic_per_hour(350, 180))
 
 print(algo.calculate())
-
-obj.blocking_rate = 0.0019
-obj.lines = False
-
-print(algo.calculate())
-
+#
+# obj.blocking_rate = 0.0019
+# obj.lines = False
+#
+# print(algo.calculate())
+#
+# obj.traffic = False
+# obj.lines = 10
+# print(algo.calculate())
 
 
 
